3ba6721c2e8265e2a4f21d45361181504dda2b1c
Sergey Nezbritskiy <sergey.nezbritskiy@gmail.com>
2018-03-11 20:18:40 +0200

    add terminals test

--- tests/Request/InfrastructureRequestTest.php
+++ tests/Request/InfrastructureRequestTest.php
@@ -28,13 +28,47 @@ class InfrastructureRequestTest extends TestCase
         $this->request = null;
     }
 
-    public function testOffices
-    ()
+    public function testATM()
     {
         $result = $this->request->execute([
             'city' => 'Днепропетровск',
             'address' => 'Титова',
             'type' => InfrastructureRequest::TYPE_ATM,
+        ]);
+        $this->assertInstanceOf(InfrastructureResponse::class, $result);
+        $data = $result->toArray();
+        $this->assertGreaterThan(0, count($data));
+        foreach ($data as $item) {
+            $this->assertArrayHasKey('type', $item);
+            $this->assertArrayHasKey('cityRU', $item);
+            $this->assertArrayHasKey('cityUA', $item);
+            $this->assertArrayHasKey('cityEN', $item);
+            $this->assertArrayHasKey('fullAddressRu', $item);
+            $this->assertArrayHasKey('fullAddressUa', $item);
+            $this->assertArrayHasKey('fullAddressEn', $item);
+            $this->assertArrayHasKey('placeRu', $item);
+            $this->assertArrayHasKey('placeUa', $item);
+            $this->assertArrayHasKey('latitude', $item);
+            $this->assertArrayHasKey('longitude', $item);
+            $this->assertArrayHasKey('working_time', $item);
+            $this->assertArrayHasKey('mon', $item['working_time']);
+            $this->assertArrayHasKey('tue', $item['working_time']);
+            $this->assertArrayHasKey('wed', $item['working_time']);
+            $this->assertArrayHasKey('thu', $item['working_time']);
+            $this->assertArrayHasKey('fri', $item['working_time']);
+            $this->assertArrayHasKey('sat', $item['working_time']);
+            $this->assertArrayHasKey('sun', $item['working_time']);
+            $this->assertArrayHasKey('hol', $item['working_time']);
+            break;
+        }
+    }
+
+    public function testTerminals()
+    {
+        $result = $this->request->execute([
+            'city' => 'Днепропетровск',
+            'address' => 'Титова',
+            'type' => InfrastructureRequest::TYPE_TERMINAL,
         ]);
         $this->assertInstanceOf(InfrastructureResponse::class, $result);
         $data = $result->toArray();
