157f9779199763a7761f75e3af4dd7899e0992f0
Sergey Nezbritskiy <sergey.nezbritskiy@gmail.com>
2018-03-15 23:12:31 +0200

    check payment response

---
+++ src/Response/CheckPaymentResponse.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace SergeyNezbritskiy\PrivatBank\Response;
+
+use SergeyNezbritskiy\PrivatBank\Base\AbstractResponse;
+
+/**
+ * Class CheckPaymentResponse
+ * @package SergeyNezbritskiy\PrivatBank\Response
+ */
+class CheckPaymentResponse extends AbstractResponse
+{
+
+    /**
+     * Response sample
+     * ```xml
+     *  <?xml version="1.0" encoding="UTF-8"?>
+     *  <response version="1.0">
+     *      <merchant>
+     *          <id>75482</id>
+     *          <signature>553995c5ccc8c81815b58cf6374f68f00a28bbd7</signature>
+     *      </merchant>
+     *      <data>
+     *          <oper>cmt</oper>
+     *          <payment id="1234567" status="ok" message="Исполнен" ref="P24A02509023364480" />
+     *      </data>
+     *  </response>
+     * ```
+     * @return array
+     */
+    protected function getMap(): array
+    {
+        return [
+            'merchant' => [
+                'id' => 'id',
+                'signature' => 'signature',
+            ],
+            'data' => [
+                'oper' => 'oper',
+                'payment' => [
+                    'id' => '@id',
+                    'status' => '@status',
+                    'message' => '@message',
+                    'ref' => '@ref',
+                ],
+            ],
+        ];
+    }
+
+}
---
+++ tests/Response/CheckPaymentResponseTest.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace SergeyNezbritskiy\PrivatBank\Tests\Response;
+
+use SergeyNezbritskiy\PrivatBank\Response\CheckPaymentResponse;
+
+/**
+ * Class CheckPaymentResponseTest
+ * @package SergeyNezbritskiy\PrivatBank\tests\Response
+ */
+class CheckPaymentResponseTest extends TestCase
+{
+
+    /**
+     * @return string
+     */
+    protected function getClass(): string
+    {
+        return CheckPaymentResponse::class;
+    }
+
+    //tests
+    public function testSuccessfulResponse()
+    {
+        $this->content = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<response version="1.0">
+    <merchant>
+        <id>75482</id>
+        <signature>553995c5ccc8c81815b58cf6374f68f00a28bbd7</signature>
+    </merchant>
+    <data>
+        <oper>cmt</oper>
+        <payment id="1234567" status="ok" message="Исполнен" ref="P24A02509023364480" />
+    </data>
+</response>
+XML;
+
+        $result = $this->response->toArray();
+        $this->assertEquals([
+            'merchant' => [
+                'id' => '75482',
+                'signature' => '553995c5ccc8c81815b58cf6374f68f00a28bbd7',
+            ],
+            'data' => [
+                'oper' => 'cmt',
+                'payment' => [
+                    'id' => '1234567',
+                    'status' => 'ok',
+                    'message' => 'Исполнен',
+                    'ref' => 'P24A02509023364480',
+                ],
+            ],
+        ], $result);
+    }
+
+}
