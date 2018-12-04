9b629e863c934d2cb865e185794f7e5a30cd1e65
Sergey Nezbritskiy <sergey.nezbritskiy@gmail.com>
2018-12-04 22:24:39 +0200

    run codesniffer with travis

--- travis.sh
+++ travis.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-vendor/bin/phpcs --standard="vendor/squizlabs/php_codesniffer/src/Standards/PSR12/ruleset.xml" ./src
-vendor/bin/phpunit
+./vendor/bin/phpcs --standard="./vendor/squizlabs/php_codesniffer/src/Standards/PSR12/ruleset.xml" ./src
+./vendor/bin/phpunit
